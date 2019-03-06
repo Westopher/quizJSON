39796d6b2a020e645debb409e6271cb6d24b30d6
West Kraemer <kraemerwest@gmail.com>
2019-03-06 11:00:10 -0500

    add quesiont struct with vars

---
+++ quizJSON/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  quizJSON
+//
+//  Created by West Kraemer on 3/6/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    var question: String?
+    var answers: [String]?
+    var correctAnswerIndex: Int?
+    var feedback: String?
+}
---
+++ quizJSON/QuizModel.swift
@@ -0,0 +1,9 @@
+//
+//  QuizModel.swift
+//  quizJSON
+//
+//  Created by West Kraemer on 3/6/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import Foundation
