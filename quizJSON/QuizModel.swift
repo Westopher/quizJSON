4fa00f72e10d299150849371732b0e874b83fc00
West Kraemer <kraemerwest@gmail.com>
2019-03-07 16:07:20 -0500

    add funcs to get json data and add filepath

--- quizJSON/QuizModel.swift
+++ quizJSON/QuizModel.swift
@@ -17,6 +17,26 @@ class QuizModel {
     var delegate:QuizProtocol?
     
     func getQuestions() {
-        delegate?.questionsRetrieved(questions: [Question]())
+       
+        getLocalJSONFile()
+        //delegate?.questionsRetrieved(questions: [Question]())
+    }
+    
+    func getLocalJSONFile() {
+        //get path to json file in app bundle
+        let path = Bundle.main.path(forResource: "QuestionData", ofType: ".json")
+        guard path != nil else {
+            print("error: can't find json file")
+            return
+        }
+        //create url object w/path
+        
+        //get data from url
+        
+        //decode data into Questions struct instances
+    }
+    
+    func getRemoteJSONFile() {
+        
     }
 }
