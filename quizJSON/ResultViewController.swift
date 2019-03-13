056d45fa01eea8dda69aecd8a308313619e043a4
West Kraemer <kraemerwest@gmail.com>
2019-03-13 19:31:57 -0400

    add iboutlets and uibutton

--- quizJSON/ResultViewController.swift
+++ quizJSON/ResultViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    @IBOutlet weak var dimView: UIView!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var feedbackLabel: UILabel!
+    
+    @IBOutlet weak var dismissButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-      
     }
+    
+    @IBAction func dismissedTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 
 }
