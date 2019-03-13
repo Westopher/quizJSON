8b934e733db70024e04fdcd1c85fee9ac97336f4
West Kraemer <kraemerwest@gmail.com>
2019-03-13 18:14:28 -0400

    add variable for numbcorrect answers

--- quizJSON/ViewController.swift
+++ quizJSON/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController, QuizProtocol, UITableViewDataSource, UIT
     var model = QuizModel()
     var questions = [Question]()
     var questionIndex = 0
+    var numCorrect = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
