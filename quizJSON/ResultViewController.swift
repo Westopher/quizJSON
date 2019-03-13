70144293da118cb645d6b069d5169c6e61d7cefe
West Kraemer <kraemerwest@gmail.com>
2019-03-13 19:01:40 -0400

    add result vc and connect to storyboard vc

---
+++ quizJSON/ResultViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultViewController.swift
+//  quizJSON
+//
+//  Created by West Kraemer on 3/13/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+}
